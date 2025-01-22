e95b4fc27568e8a24408a6c70022c8a2cf784ace
Rajveer75way <rajveer.75way@gmail.com>
2025-01-22 10:05:35 +0530

    Delete app/modules directory

--- app/modules/financialReport/financial.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { generateAndSendReport } from './financial.controller'; // Import the controller
-
-const router = Router();
-
-router.post('/generate-financial-report', generateAndSendReport);
-
-export default router;
